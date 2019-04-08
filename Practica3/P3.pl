6ae043e60cb92ec9a926e7a0ef9ba3ad17afd22b
Javier López <javier.lopezcano@estudiante.uam.es>
2019-04-08 22:53:42 +0100

    Update ej 8 WIP

--- Practica3/P3.pl
+++ Practica3/P3.pl
@@ -1,39 +1,38 @@
-### Ejercicio 1
+% Ejercicio 1
 
-```
 duplica([], []).
 duplica([X|T], [X, X|L]):-duplica(T, L).
-```
 
-### Ejercicio 2
+% Ejercicio 2
 
-```
+
 concatena([], L, L).
 concatena([X|L1], L2, [X|L3]):-concatena(L1, L2, L3).
 
 invierte([], []).
 invierte([H|T],L):-invierte(T,R),concatena(R,[H],L).
-```
 
-### Ejercicio 3
-Basta con llamar a función *invierte* creada anteriormente. La copiamos de nuevo pues, al estar en otro cuadro de programa, swish-prolog no lo detecta.
-Si se llama con una variable no instanciada, genera la lista vacía, pero pulsando los botones *Next*, *10*, *100*... que aparecen, se muestran nuevos ejemplos con listas que incluyes posiciones de memoria, que son palíndromos. En nuestro ejemplo, generando 10 más:
 
-```
-X = []
-X = [_1164]
-X = [_1192, _1192]
-X = [_1226, _1232, _1226]
-X = [_1266, _1272, _1272, _1266]
-X = [_1312, _1318, _1324, _1318, _1312]
-X = [_1364, _1370, _1376, _1376, _1370, _1364]
-X = [_1422, _1428, _1434, _1440, _1434, _1428, _1422]
-X = [_1486, _1492, _1498, _1504, _1504, _1498, _1492, _1486]
-X = [_832, _838, _844, _850, _856, _850, _844, _838, _832]
-X = [_908, _914, _920, _926, _932, _932, _926, _920, _914, _908]
-```
+% Ejercicio 3
 
-```
+%Basta con llamar a función *invierte* creada anteriormente. La copiamos de nuevo pues, al estar en otro cuadro de programa, swish-prolog no lo detecta.
+%Si se llama con una variable no instanciada, genera la lista vacía, pero pulsando los botones *Next*, *10*, *100*... que aparecen, se muestran nuevos ejemplos con listas que incluyes posiciones de memoria, que son palíndromos. En nuestro ejemplo, generando 10 más:
+
+
+%X = []
+%X = [_1164]
+%X = [_1192, _1192]
+%X = [_1226, _1232, _1226]
+%X = [_1266, _1272, _1272, _1266]
+%X = [_1312, _1318, _1324, _1318, _1312]
+%X = [_1364, _1370, _1376, _1376, _1370, _1364]
+%X = [_1422, _1428, _1434, _1440, _1434, _1428, _1422]
+%X = [_1486, _1492, _1498, _1504, _1504, _1498, _1492, _1486]
+%X = [_832, _838, _844, _850, _856, _850, _844, _838, _832]
+%X = [_908, _914, _920, _926, _932, _932, _926, _920, _914, _908]
+
+
+
 concatena([], L, L).
 concatena([X|L1], L2, [X|L3]):-concatena(L1, L2, L3).
 
@@ -41,23 +40,23 @@ invierte([], []).
 invierte([H|T],L):-invierte(T,R),concatena(R,[H],L).
 
 palindromo(L):-invierte(L, L).
-```
 
-### Ejercicio 4
 
-```
+% Ejercicio 4
+
+
 concatena([], L, L).
 concatena([X|L1], L2, [X|L3]):-concatena(L1, L2, L3).
 
 divide([X|T],1,[X],T).
 divide([X|T],N,L1,L2):-N2 is N-1, divide(T, N2, H, L2), concatena([X], H, L1).
-```
 
-### Ejercicio 5
 
-En el caso de ponerlo al reves, como *aplasta(R,  [a, b, c, d]).* la función devuelve false, porque no es ninguno de los casos base
+% Ejercicio 5
 
-```
+%En el caso de ponerlo al reves, como *aplasta(R,  [a, b, c, d]).* la función devuelve false, porque no es ninguno de los casos base
+
+
 aplasta([], []) :- !.
 aplasta([X|T], Res) :-
     !,
@@ -66,24 +65,24 @@ aplasta([X|T], Res) :-
     append(L1, L2, Res).
 aplasta(L, [L]).
 
-```
 
-### Ejercicio 6 
 
-Ejemplos: 
-next_factor(5, 2, NF) 3, el caso base
-next_factor(2, 5, NF) FALSE porque 5 &lt; sqrt(2)
-next_factor(2, 1, NF) 3
-next_factor(25, 3, NF) 5
-next_factor(25, 5, NF) false
+% Ejercicio 6 
 
-primos(1, []) true, el caso base
-primos(4, [2, 2]) true
-primos(4, [2, 3]) false
-primos(63, [3, 3, 7]) true
-primos(63, [3, 3, 8]) false
-primos(63, L) [3, 3, 7]
-primos(4, L) [2, 2]
+%Ejemplos: 
+%next_factor(5, 2, NF) 3, el caso base
+%next_factor(2, 5, NF) FALSE porque 5 &lt; sqrt(2)
+%next_factor(2, 1, NF) 3
+%next_factor(25, 3, NF) 5
+%next_factor(25, 5, NF) false
+
+%primos(1, []) true, el caso base
+%primos(4, [2, 2]) true
+%primos(4, [2, 3]) false
+%primos(63, [3, 3, 7]) true
+%primos(63, [3, 3, 8]) false
+%primos(63, L) [3, 3, 7]
+%primos(4, L) [2, 2]
 
 next_factor(_, 2, 3).
 next_factor(N, F, NF):- F < sqrt(N), NF is F + 2.
@@ -106,9 +105,9 @@ primos(N, L) :-
 
 
 
-### Ejercicio 8
+% Ejercicio 8
 
-#### 8.1
+% 8.1
 
 concatena2(X, Y, X-Y).	
 
@@ -123,7 +122,7 @@ build_tree([X|Rs], Y):-
 
 
 
-#### 8.2
+% 8.2
 
 encode_elem(X, Y, T):-
     Y = [],
@@ -139,7 +138,7 @@ encode_elem(X, [0], T):-
 
 
 
-#### 8.3
+% 8.3
 
 encode_list([X], [Y], T):-
     encode_elem(X, Y, T).
@@ -149,7 +148,7 @@ encode_list([X|R1], [Y|R2], T):-
 
 
 
-#### 8.4 -Aún no funciona.
+% 8.4 -Aún no funciona.
 
 number_times(X, [], [X, 0]).
 number_times(X, [X|R], [X, N]):-
@@ -158,12 +157,23 @@ number_times(X, [X|R], [X, N]):-
 number_times(X, [_|R], [X, N]):-
     number_times(X, R, [X, N]).
 
+
+format_list(L1, L2):-
+	sort(L1, Aux),
+	
+
+
+
 diccionario([a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]).
 
-%ordena(L1, L2):
-    % comprobar que los elementos de L1 están en diccionario.
-    % eliminar duplicados L1
-    % ordenar la lista sin duplicados en L2 según el número de veces (number_times()) que el caracter está en L1.
+
+ordena(L1, L2):-
+	sort(L1, Aux),
+	times_list(L1, Aux),
+	sort(2, @>=, Aux, L2).
+    %comprobar que los elementos de L1 están en diccionario.
+    % aplastar L1
+    % ordenar la lista aplastada en L2 según el número de veces (number_times()) que el caracter está en L1.
 
 encode(L1, L2):-
     ordena(L1, L),
